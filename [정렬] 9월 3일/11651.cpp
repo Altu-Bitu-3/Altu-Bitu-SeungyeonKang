e3c0db064ebc6b143dafe733f94044dccdf8abd9
Kang seungyeon <77188916+KangSYeon@users.noreply.github.com>
2022-09-05 11:03:26 +0900

    [정렬] 9월 3일(수정)
    11651 수정해서 다시 올립니다!

--- [정렬] 9월 3일/11651.cpp
+++ [정렬] 9월 3일/11651.cpp
@@ -1,43 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-vector<vector<int>> arr;
-
-void BubbleSort(int n) {
-
-	for (int i = 0; i < n - 1; i++) {
-		int isSwap = false;
-		for (int j = 0; j < n - 1; j++) {
-			if (arr[j][1] > arr[j + 1][1]) {
-				swap(arr[j], arr[j + 1]);
-				isSwap = true;
-			}
-		}
-
-		if (!isSwap) {
-			break;
-		}
-	}
-
-}
 
 int main() {
 
 	int n = 0;
 	cin >> n;
-
+	vector<vector<int>> arr;
 	arr.assign(n, vector<int>(2, 0));
 
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i][0];
 		cin >> arr[i][1];
+		cin >> arr[i][0];
 	}
 
-	BubbleSort(n);
+	sort(arr.begin(), arr.end());
 
 	for (int i = 0; i < n; i++) {
-		cout << arr[i][0] << " " << arr[i][1] << '\n';
+		cout << arr[i][1] << " " << arr[i][0] << '\n';
 	}
 
 	return 0;
